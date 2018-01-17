65dbd963aea4ac701fc4c2c54e67a62f1ef5dc07
wsyshengyun <wsy8718@163.com>
2018-01-17 17:24:55 +0800

    start sqlalchemy ...

---
+++ foo/new.py
@@ -0,0 +1,7 @@
+# coding:utf8
+from sqlalchemy import Column, String, Text, Integer, create_engine
+from sqlalchemy.orm import session_maker, relationship
+from sqlalchemy.ext.declarative import declarative_base()
+
+engine =
+
